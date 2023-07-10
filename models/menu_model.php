915d75cef269d82c875a25da3e5540b04fc76d27
duonghuupha <duonghuupha@hotmail.com.vn>
2023-07-11 01:22:21 +0700

    time 4

---
+++ models/menu_model.php
@@ -0,0 +1,38 @@
+<?php
+class Menu_Model extends Model{
+    function __construct(){
+        parent::__construct();
+    }
+
+    function getFetObj($offset, $rows){
+        $result = array();
+        $query = $this->db->query("SELECT COUNT(*) AS Total FROM tbl_menu");
+        $row = $query->fetchAll();
+        $query = $this->db->query("SELECT id, title, description, image, status, create_at FROM tbl_menu
+                                    ORDER BY id DESC LIMIT $offset, $rows");
+        $result['total'] = $row[0]['Total'];
+        $result['rows'] = $query->fetchAll();
+        return $result;
+    }
+
+    function addObj($data){
+        $query = $this->insert("tbl_menu", $data);
+        return $query;
+    }  
+
+    function updateObj($id, $data){
+        $query = $this->update("tbl_menu", $data, "id = $id");
+        return $query;
+    }
+
+    function delObj($id){
+        $query = $this->delete("tbl_menu", "id = $id");
+        return $query;
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    function get_data_blog($q, $offset, $rows){
+        $result = array();
+        $query = $this->db->query("SELECT COUNT(*) AS Total FROM tbl_content WHERE status = 1 AND title LIKE '%$q%'");
+    }
+}
+?>
